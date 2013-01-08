1a09668cf3f60fab495e21ec8f29b6f4eace5e55
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2013-01-08 22:34:08 +0100

    epicboard shows only epics like product backlog would - project.self and descendants
    Conflicts:
    
    	app/controllers/rb_epicboards_controller.rb

--- app/controllers/rb_epicboards_controller.rb
+++ app/controllers/rb_epicboards_controller.rb
@@ -1,54 +1,47 @@
 include RbCommonHelper
 
+class FakeProductBacklog
+  def id; 0 end
+  def name; 'Product Backlog' end
+end
+
 class RbEpicboardsController < RbApplicationController
   unloadable
 
   def show
-    @projects = @project.projects_in_shared_product_backlog #self or self and descendants
-    stories = RbStory.in_projects(@projects).stories.visible.order(:position)
-    @epics = RbEpic.in_projects(@projects).epics.visible.order(:position)
-    @story_ids    = stories.map{|s| s.id}
+    cls = RbStory
+    product_backlog_stories = cls.product_backlog(@project)
+    @product_backlog = { :sprint => FakeProductBacklog.new, :stories => product_backlog_stories||[] }
 
-    @settings = Backlogs.settings
+    sprints = @project.open_shared_sprints
+    sprints_backlog_storie_of = cls.backlogs_by_sprint(@project, sprints)
+    @sprint_backlogs = sprints.map{ |s| { :sprint => s, :stories => sprints_backlog_storie_of[s.id]||[] } }
 
-    ## determine status columns to show
-    tracker = Tracker.find_by_id(RbStory.trackers[0])
-    statuses = tracker.issue_statuses
-    # disable columns by default
-    if User.current.admin? || stories.length == 0
-      @statuses = statuses
-    else
-      enabled = {}
-      statuses.each{|s| enabled[s.id] = false}
-      # enable all statuses held by current tasks, regardless of whether the current user has access
-      stories.each {|task| enabled[task.status_id] = true }
-      enabled[IssueStatus.default.id] = true if stories.length == 0 #enable at least one if there are no stories
+    releases = @project.open_releases_by_date
+    releases_backlog_storie_of = cls.backlogs_by_release(@project, releases)
+    @release_backlogs = releases.map{ |r| { :sprint => r, :stories => releases_backlog_storie_of[r.id]||[] } }
 
-      roles = User.current.roles_for_project(@project)
-      #@transitions = {}
-      statuses.each {|status|
+    #This project
+    #@epics = RbEpic.in_projects([@project]).epics.visible.order(:position)||[]
 
-        # enable all statuses the current user can reach from any task status
-        [false, true].each {|creator|
-          [false, true].each {|assignee|
+    #This project and subprojects
+    @epics = RbEpic.epics.visible.
+                open.
+                order('issues.position').
+                in_projects(@project.projects_in_shared_product_backlog)||[]
+    #add All relevant from sprints and releases
+    #sprints.each{|sprint|
+    #  @epics += RbEpic.backlog(sprint.project.id, sprint.id, nil, {})||[]
+    #}
+    #releases.each{|release|
+    #  @epics += RbEpic.backlog(release.project.id, nil, release.id {})||[]
+    #}
+    #@epics make unique and sort by position
 
-            allowed = status.new_statuses_allowed_to(roles, tracker, creator, assignee).collect{|s| s.id}
-            #@transitions["c#{creator ? 'y' : 'n'}a#{assignee ? 'y' : 'n'}"] = allowed
-            allowed.each{|s| enabled[s] = true}
-          }
-        }
-      }
-      @statuses = statuses.select{|s| enabled[s.id]}
-    end
-
-    if stories.size == 0
-      @last_updated = nil
-    else
-      @last_updated = RbStory.find(:first,
-                        :conditions => ['tracker_id in (?)', RbStory.trackers],
-                        :order      => "updated_on DESC")
-    end
-
+    @columns = [@product_backlog,
+                @release_backlogs,
+                @sprint_backlogs
+               ].flatten
     respond_to do |format|
       format.html { render :layout => "rb" }
     end
