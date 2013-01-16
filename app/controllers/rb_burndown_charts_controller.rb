bbb0440d0218c3bad42cf7e202ed6abd24e20d9f
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2013-01-16 19:10:10 +0100

    solve some rails deprecation warnings about template handlers and formats

--- app/controllers/rb_burndown_charts_controller.rb
+++ app/controllers/rb_burndown_charts_controller.rb
@@ -11,7 +11,7 @@ class RbBurndownChartsController < RbApplicationController
 
   def embedded
     respond_to do |format|
-      format.html { render :template => 'rb_burndown_charts/show.html.erb', :layout => false }
+      format.html { render :template => 'rb_burndown_charts/show', :formats => [:html], :layout => false }
     end
   end
 
