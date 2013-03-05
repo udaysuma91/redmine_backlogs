55e5204acd08b6429a170c5730d9720ae5e6efd3
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2013-03-05 20:46:53 +0000

    scoping open_sprints,closed_sprints, more code cleanup

--- lib/backlogs_project_patch.rb
+++ lib/backlogs_project_patch.rb
@@ -187,7 +187,7 @@ module Backlogs
       end
 
       def rb_project_settings
-        RbProjectSettings.where(:project_id => self.id]).first_or_create
+        RbProjectSettings.where(:project_id => self.id).first_or_create #cool magic that is
       end
 
       def projects_in_shared_product_backlog
@@ -207,9 +207,8 @@ module Backlogs
       # 3. share to project but are scoped to project and subprojects
       #depending on sharing mode
       def open_shared_sprints
-        if Backlogs.setting[:sharing_enabled]
-          order = Backlogs.setting[:sprint_sort_order] == 'desc' ? 'DESC' : 'ASC'
-          shared_versions.visible.scoped(:conditions => {:status => ['open', 'locked']}, :order => "sprint_start_date #{order}, effective_date #{order}").collect{|v| v.becomes(RbSprint) }
+        if Backlogs.setting[:sharing_enabled] #FIXME adapt api like open_releases_by_date
+          shared_sprints.visible.where(:status => ['open', 'locked']).by_date
         else #no backlog sharing
           RbSprint.open_sprints(self)
         end 
@@ -217,43 +216,68 @@ module Backlogs
 
       #depending on sharing mode
       def closed_shared_sprints
-        if Backlogs.setting[:sharing_enabled]
-          order = Backlogs.setting[:sprint_sort_order] == 'desc' ? 'DESC' : 'ASC'
-          shared_versions.visible.scoped(:conditions => {:status => ['closed']}, :order => "sprint_start_date #{order}, effective_date #{order}").collect{|v| v.becomes(RbSprint) }
+        if Backlogs.setting[:sharing_enabled] #FIXME adapt api like open_releases_by_date
+          shared_sprints.visible.where(:status => 'closed').by_date
         else #no backlog sharing
           RbSprint.closed_sprints(self)
         end
       end
 
       def active_sprint
-        @active_sprint ||= RbSprint.find(:first, :conditions => [
-          "project_id = ? and status = 'open' and not (sprint_start_date is null or effective_date is null) and ? between sprint_start_date and effective_date",
+        @active_sprint ||= RbSprint.where([
+          "project_id = ?
+           and status = 'open'
+            and not (sprint_start_date is null or effective_date is null)
+            and ? between sprint_start_date and effective_date",
           self.id, (Time.zone ? Time.zone : Time).now.beginning_of_day
-        ])
+        ]).first
       end
 
       def open_releases_by_date
-        order = Backlogs.setting[:sprint_sort_order] == 'desc' ? 'DESC' : 'ASC'
+        dir = Backlogs.setting[:sprint_sort_order] == 'desc' ? 'DESC' : 'ASC'
         (Backlogs.setting[:sharing_enabled] ? shared_releases : releases).
-          visible.open.
-          order("#{RbRelease.table_name}.release_start_date #{order}, #{RbRelease.table_name}.release_end_date #{order}")
+          visible.open. #FIXME implement RbRelease::by_date scope
+          order("#{RbRelease.table_name}.release_start_date #{dir}, #{RbRelease.table_name}.release_end_date #{dir}")
+      end
+
+      def shared_sprints
+        #like shared_versions but we operate on RbSprint to avoid becomes() and to prepare separate Sprint model
+        if new_record?
+          RbSprint.includes(:project).
+            where("#{Project.table_name}.status <> #{Project::STATUS_ARCHIVED}
+                  AND #{RbSprint.table_name}.sharing = 'system'")
+        else
+          @shared_versions ||= begin
+            r = root? ? self : root
+            RbSprint.includes(:project).
+              where("#{Project.table_name}.id = #{id}" +
+                    " OR (#{Project.table_name}.status <> #{Project::STATUS_ARCHIVED} AND (" +
+                    " #{RbSprint.table_name}.sharing = 'system'" +
+                    " OR (#{Project.table_name}.lft >= #{r.lft} AND #{Project.table_name}.rgt <= #{r.rgt} AND #{RbSprint.table_name}.sharing = 'tree')" +
+                    " OR (#{Project.table_name}.lft < #{lft} AND #{Project.table_name}.rgt > #{rgt} AND #{RbSprint.table_name}.sharing IN ('hierarchy', 'descendants'))" +
+                    " OR (#{Project.table_name}.lft > #{lft} AND #{Project.table_name}.rgt < #{rgt} AND #{RbSprint.table_name}.sharing = 'hierarchy')" +
+                    "))")
+      end
+    end
+      
       end
 
       def shared_releases
         if new_record?
-          RbRelease.scoped(:include => :project,
-                       :conditions => "#{Project.table_name}.status <> #{Project::STATUS_ARCHIVED} AND #{RbRelease.table_name}.sharing = 'system'")
+          RbRelease.includes(:project).
+            where("#{Project.table_name}.status <> #{Project::STATUS_ARCHIVED}
+                  AND #{RbRelease.table_name}.sharing = 'system'")
         else
           @shared_releases ||= begin
             r = root? ? self : root
-            RbRelease.scoped(:include => :project,
-                         :conditions => "#{Project.table_name}.id = #{id}" +
-              " OR (#{Project.table_name}.status <> #{Project::STATUS_ARCHIVED} AND (" +
-                    " #{RbRelease.table_name}.sharing = 'system'" +
-              " OR (#{Project.table_name}.lft >= #{r.lft} AND #{Project.table_name}.rgt <= #{r.rgt} AND #{RbRelease.table_name}.sharing = 'tree')" +
-              " OR (#{Project.table_name}.lft < #{lft} AND #{Project.table_name}.rgt > #{rgt} AND #{RbRelease.table_name}.sharing IN ('hierarchy', 'descendants'))" +
-              " OR (#{Project.table_name}.lft > #{lft} AND #{Project.table_name}.rgt < #{rgt} AND #{RbRelease.table_name}.sharing = 'hierarchy')" +
-              "))")
+            RbRelease.includes(:project).
+              where("#{Project.table_name}.id = #{id}" +
+                    " OR (#{Project.table_name}.status <> #{Project::STATUS_ARCHIVED} AND (" +
+                          " #{RbRelease.table_name}.sharing = 'system'" +
+                    " OR (#{Project.table_name}.lft >= #{r.lft} AND #{Project.table_name}.rgt <= #{r.rgt} AND #{RbRelease.table_name}.sharing = 'tree')" +
+                    " OR (#{Project.table_name}.lft < #{lft} AND #{Project.table_name}.rgt > #{rgt} AND #{RbRelease.table_name}.sharing IN ('hierarchy', 'descendants'))" +
+                    " OR (#{Project.table_name}.lft > #{lft} AND #{Project.table_name}.rgt < #{rgt} AND #{RbRelease.table_name}.sharing = 'hierarchy')" +
+                    "))")
           end
         end
       end
