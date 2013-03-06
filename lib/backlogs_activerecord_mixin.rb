c10dc24eb2f7313954ee37c032d5152906838d5c
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2013-03-06 22:46:13 +0000

    remove obsolete module function ActiveRecord.add_condition

--- lib/backlogs_activerecord_mixin.rb
+++ lib/backlogs_activerecord_mixin.rb
@@ -1,20 +1,5 @@
 module Backlogs
   module ActiveRecord
-    def add_condition(options, condition, conjunction = 'AND')
-      if condition.is_a? String
-        add_condition(options, [condition], conjunction)
-      elsif condition.is_a? Hash
-        add_condition!(options, [condition.keys.map { |attr| "#{attr}=?" }.join(' AND ')] + condition.values, conjunction)
-      elsif condition.is_a? Array
-        options[:conditions] ||= []
-        options[:conditions][0] += " #{conjunction} (#{condition.shift})" unless options[:conditions].empty?
-        options[:conditions] = options[:conditions] + condition
-      else
-        raise "don't know how to handle this condition type"
-      end
-    end
-    module_function :add_condition
-
     module Attributes
       def self.included receiver
         receiver.extend ClassMethods
