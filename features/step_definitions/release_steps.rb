082d838a6f31d42a22e8018fe963b31bead28e7b
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2013-03-07 13:54:35 +0000

    code cleanup test release_steps

--- features/step_definitions/release_steps.rb
+++ features/step_definitions/release_steps.rb
@@ -1,18 +1,18 @@
 Then /^show me the releases$/ do
-  RbRelease.find(:all).each{|release|
+  RbRelease.find_each{|release|
     puts "Release: #{release}"
   }
 end
 
 Then /^show me release (.+)$/ do |release_name|
-  release = RbRelease.find_by_name(release_name)
+  release = RbRelease.where(:name => release_name).first
   release.should_not be_nil
   puts "Release: #{release}"
   release.issues.each{|issue| puts "  Issue: #{issue}" }
 end
 
 Then /^show me the release backlog of (.+)$/ do |release_name|
-  release = RbRelease.find_by_name(release_name)
+  release = RbRelease.where(:name => release_name).first
   release.should_not be_nil
   RbStory.release_backlog(release).each{|issue|
     puts "  #{issue}"
@@ -20,10 +20,10 @@ Then /^show me the release backlog of (.+)$/ do |release_name|
 end
 
 When /^I add story (.+) to release (.+)$/ do |story_name, release_name|
-  story = RbStory.find_by_subject(story_name)
+  story = RbStory.where(:subject => story_name).first
   @story_params = {
     :id => story.id,
-    :release_id => RbRelease.find_by_name(release_name).id
+    :release_id => RbRelease.where(:name => release_name).first.id
   }
   page.driver.post(
                       url_for(:controller => :rb_stories,
@@ -37,39 +37,39 @@ When /^I add story (.+) to release (.+)$/ do |story_name, release_name|
 end
 
 Then /^story (.+) should belong to release (.+)$/ do |story_name, release_name|
-  release = RbRelease.find_by_name(release_name)
+  release = RbRelease.where(:name => release_name).first
   release.should_not be_nil
-  story = RbStory.find_by_subject(story_name)
+  story = RbStory.where(:subject => story_name).first
   story.should_not be_nil
   release.issues.exists?(story).should be_true
 end
 
 Then /^story (.+) should not belong to any release$/ do |story_name|
-  story = RbStory.find_by_subject(story_name)
+  story = RbStory.where(:subject => story_name).first
   story.should_not be_nil
   story.release_id.should be_nil
 end
 
 Then /^I should see the release backlog of (.+)$/ do |release|
-  release = RbRelease.find_by_name(release)
+  release = RbRelease.where(:name => release).first
   release.should_not be_nil
   page.should have_css("#stories-for-release-#{release.id}")
 end
 
 Then /^I should see (\d+) stories in the release backlog of (.+)$/ do |count, release|
-  release = RbRelease.find_by_name(release)
+  release = RbRelease.where(:name => release).first
   release.should_not be_nil
   page.all(:css, "#stories-for-release-#{release.id} .story").length.should == count.to_i
 end
 
 Then /^release "([^"]*)" should have (\d+) story points$/ do |release, points|
-  release = RbRelease.find_by_name(release)
+  release = RbRelease.where(:name => release).first
   release.should_not be_nil
   release.remaining_story_points.should == points.to_f
 end
 
 Then /^The release "([^"]*)" should be closed$/ do |release|
-  release = RbRelease.find_by_name(release)
+  release = RbRelease.where(:name => release).first
   release.status.should == 'closed'
   release.closed?.should be_true
 end
@@ -78,7 +78,7 @@ Given /^I have made the following story mutations:$/ do |table|
   #Mutations happen at 'day' relative to the story's sprint
   table.hashes.each do |mutation|
     mutation.delete_if{|k, v| v.to_s.strip == '' }
-    story = RbStory.find_by_subject(mutation.delete('story'))
+    story = RbStory.where(:subject => mutation.delete('story')).first
     story.should_not be_nil
     current_sprint(story.fixed_version.name)
     set_now(mutation.delete('day'), :msg => story.subject, :sprint => current_sprint)
@@ -88,7 +88,7 @@ Given /^I have made the following story mutations:$/ do |table|
     if status_name.blank?
       status = nil
     else
-      status = IssueStatus.find(:first, :conditions => ['name = ?', status_name])
+      status = IssueStatus.where(:name => status_name).first
       raise "No such status '#{status_name}'" unless status
       status = status.id
     end
@@ -101,13 +101,13 @@ Given /^I have made the following story mutations:$/ do |table|
 end
 
 Then /^release "([^"]*)" should have (\d+) sprints$/ do |release, num|
-  release = RbRelease.find_by_name(release)
+  release = RbRelease.where(:name => release).first
   release.should_not be_nil
   release.sprints.size.should == num.to_i
 end
 
 Then /^show me the burndown data for release "([^"]*)"$/ do |release|
-  release = RbRelease.find_by_name(release)
+  release = RbRelease.where(:name => release).first
   burndown = release.burndown
   puts "added     #{burndown[:added_points]}"
   puts "added_pos #{burndown[:added_points_pos]}"
@@ -118,7 +118,7 @@ Then /^show me the burndown data for release "([^"]*)"$/ do |release|
 end
 
 Then /^the release burndown for release "([^"]*)" should be:$/ do |release, table|
-  release = RbRelease.find_by_name(release)
+  release = RbRelease.where(:name => release).first
   burndown = release.burndown
   table.hashes.each do |metrics|
     sprint = metrics.delete('sprint')
@@ -135,8 +135,8 @@ Then /^the release burndown for release "([^"]*)" should be:$/ do |release, tabl
 end
 
 Then /^journal for "([^"]*)" should show change to release "([^"]*)"$/ do |story_name,release_name|
-  release = RbRelease.find_by_name(release_name)
-  story = RbStory.find_by_subject(story_name)
+  release = RbRelease.where(:name => release_name).first
+  story = RbStory.where(:subject => story_name).first
   found_change = false
   # Find journal entry containing change to release
   story.journals.each{|journal|
