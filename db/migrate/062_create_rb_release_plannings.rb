76684f6a7a3172783744fe72d28d5d6ba05e82cd
Uday Magdum <uday.magdum@sumasoft.net>
2018-09-06 18:49:49 +0530

    commented migration because already migrated

--- db/migrate/062_create_rb_release_plannings.rb
+++ db/migrate/062_create_rb_release_plannings.rb
@@ -1,9 +1,9 @@
 class CreateRbReleasePlannings < ActiveRecord::Migration
   def change
-    create_table :rb_release_plannings do |t|
-      t.string :name
-      t.text :description
-      t.belongs_to :project
-    end
+    # create_table :rb_release_plannings do |t|
+    #   t.string :name
+    #   t.text :description
+    #   t.belongs_to :project
+    # end
   end
 end
